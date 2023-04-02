183fc35b20a5bc5956421c9c3d46f5b2d07fbe59
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 17:07:48 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+class Program {
+    static void Main(string[] args) {
+        int delay = 220;
+
+        string melody = "CDEFFFCDCDDEEEFF";
+
+        for (int i = 0; i < melody.Length; i++) {
+            char note = melody[i];
+
+            switch (note) {
+                case 'C':
+                    Console.Beep(261, 500); // C
+                    break;
+                case 'D':
+                    Console.Beep(293, 500); // D
+                    break;
+                case 'E':
+                    Console.Beep(329, 500); // E
+                    break;
+                case 'F':
+                    Console.Beep(349, 500); // F
+                    break;
+            }
+
+            Console.WriteLine(note);
+            Thread.Sleep(delay);
+        }
+    }
+}
